f00262518acc5e7659a63adc0314156303ec1f76
mikakane <mikakane2@gmail.com>
2016-07-28 13:08:21 +0900

    udpate

--- README.md
+++ README.md
@@ -15,11 +15,21 @@ and register in [peridot.php](http://peridot-php.github.io/#plugins) file
 ````
 <?php
 use Evenement\EventEmitterInterface;
-use Peridot\Plugin\HttpKernel\HttpKernelPlugin;
 
 return function(EventEmitterInterface $emitter) {
-    //the second argument expects an HttpKernelInterface or a function that returns one
-    HttpKernelPlugin::register($emitter, include __DIR__ . '/app.php');
+    \Peridot\Plugin\Lumen\register(require __DIR__ . '/app.php');
 };
 ````
 
+You can use it in your spec as `$this->lumen`
+
+````
+    it('GET /string should get string ', function() {
+        $this->lumen->get("/string");
+        /** @var \Illuminate\Http\Response $response */
+        $response = $this->lumen->response;
+
+        assert($response->getStatusCode() === 200);
+        assert($response->getOriginalContent() === "hogehoge");
+    });
+````
--- specs/app.spec.php
+++ specs/app.spec.php
@@ -10,7 +10,7 @@ describe('Application', function() {
     it('GET /string should get string ', function() {
         $this->lumen->get("/string");
         /** @var \Illuminate\Http\Response $response */
-        $response = $this->lumen->response();
+        $response = $this->lumen->response;
 
         assert($response->getStatusCode() === 200);
         assert($response->getOriginalContent() === "hogehoge");
@@ -19,7 +19,7 @@ describe('Application', function() {
     it('GET /array should get array', function() {
         $this->lumen->get("/array");
         /** @var \Illuminate\Http\Response $response */
-        $response = $this->lumen->response();
+        $response = $this->lumen->response;
 
         assert($response->getStatusCode() === 200);
         assert($response->getOriginalContent() === ["apple","lemon","banana"]);
@@ -27,7 +27,7 @@ describe('Application', function() {
     it('should get 404 ', function() {
         $this->lumen->get("/hogehoge");
         /** @var \Illuminate\Http\Response $response */
-        $response = $this->lumen->response();
+        $response = $this->lumen->response;
 
         assert($response->getStatusCode() === 404);
     });
